4cb5b6263d1b02af6cddd940b2a39a5dc8247ff3
Om Pandey <ompandey.9721@gmail.com>
2025-08-10 13:17:34 +0530

    Create Two Sum.cpp
    //Leet Code Q.1

---
+++ Two Sum.cpp
@@ -0,0 +1,24 @@
+//Platform - LeetCode Q.1
+
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // Should not happen given the problem constraints
+    }
+};
